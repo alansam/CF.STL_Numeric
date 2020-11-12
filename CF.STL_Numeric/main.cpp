6172602c49378a98d4418831a49890739383b0fc
Alan Sampson <alansamps@gmail.com>
2020-11-11 20:57:34 -0800

    Initial Commit

---
+++ CF.STL_Numeric/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CF.STL_Numeric
+//
+//  Created by Alan Sampson on 11/11/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
